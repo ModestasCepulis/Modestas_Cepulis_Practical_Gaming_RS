4bb88b7e1e5b59842e7f67eb561311afdf8a08ec
Modestas Cepulis <Modestas.Cepulis@students.ittralee.ie>
2019-02-13 12:42:49 +0000

    did some enums
    Signed-off-by: Modestas Cepulis <Modestas.Cepulis@students.ittralee.ie>

---
+++ Unity Project/Overgrown/Assets/Scripts/InstantiateObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObjects : MonoBehaviour {
+
+    public GameObject tomatoeBarrel;
+    public GameObject potatoeBarrel;
+
+    public Transform tomatoePosition;
+    public Transform potatoePosition;
+
+    // Use this for initialization
+    void Start () {
+
+        //tomatoeBarrel = PlantsController.PlantType.Tomatoe;
+        //potatoeBarrel = PlantsController.PlantType.Potatoe;
+
+        Instantiate(tomatoeBarrel, tomatoePosition.position, Quaternion.identity);
+        Instantiate(potatoeBarrel, potatoePosition.position, Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
